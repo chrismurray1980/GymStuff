785d75fa3a75aff4711b76771071ca465ef49f2d
EC2 Default User <ec2-user@ip-172-31-6-37.eu-west-1.compute.internal>
2020-07-22 09:42:47 +0000

    added category field to products

--- products/models.py
+++ products/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 
+CATEGORY = (
+    ('Weights', 'Weights'),
+    ('Benches', 'Benches'),
+    ('Sets', 'Sets'),
+    ('Powders', 'Powders'),
+    ('Vitamins', 'Vitamins'),)
+
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=254, default='')
+    category = models.CharField(max_length=30, choices=CATEGORY, default='Weights')
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='images')
