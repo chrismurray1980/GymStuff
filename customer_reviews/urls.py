a7cf49f24a906cf2f4918a2a6afb956c4c89b039
EC2 Default User <ec2-user@ip-172-31-6-37.eu-west-1.compute.internal>
2020-07-23 13:50:06 +0000

    linked reviews to specific product and added link in url

--- customer_reviews/urls.py
+++ customer_reviews/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url, include
 from .views import customer_reviews, review_detail
 
 urlpatterns = [
-    url(r'^$', customer_reviews, name='customer_reviews'),
-    url(r'^(?P<cusomer_review_id>[0-9]+)/$', review_detail, name='review_detail'),
+    url(r'^(?P<id>\d)/$', customer_reviews, name='customer_reviews'),
+    url(r'^(?P<customer_review_id>[0-9]+)/$', review_detail, name='review_detail'),
 ]
--- customer_reviews/views.py
+++ customer_reviews/views.py
@@ -5,12 +5,13 @@ from .models import customer_review
 from products.models import Product
 import datetime
 
-def customer_reviews(request):
-    latest_review_list = customer_review.objects.order_by('-date')[:9]
+def customer_reviews(request, id):
+    #autors = Autor.objects.filter(recolha__categoria__categoria='my_category_name')
+    latest_review_list = customer_review.objects.filter(product__id= id)
+    #.order_by('-date')[:9]
     context = {'latest_review_list':latest_review_list}
     return render(request, 'customer_reviews.html', context)
-
-
+                
 def review_detail(request, customer_review_id):
     review = get_object_or_404(customer_review, pk=customer_review_id)
     return render(request, 'review_detail.html', {'review': review})
