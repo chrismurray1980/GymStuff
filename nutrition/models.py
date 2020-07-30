1be178a2e6c694fafbb95ec261e5e088f7f68182
EC2 Default User <ec2-user@ip-172-31-6-37.eu-west-1.compute.internal>
2020-07-30 11:56:23 +0000

    added obesity category

--- nutrition/models.py
+++ nutrition/models.py
@@ -7,7 +7,4 @@ class Physical(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,)
     height = models.FloatField()
     weight = models.FloatField()
-    
-    
-    def bmi(self):
-        return self.weight_in_kg / self.height_in_meters ** 2
+
--- nutrition/views.py
+++ nutrition/views.py
@@ -15,19 +15,33 @@ def bmi_form(request):
         physical = Physical()
         physical.height= height
         physical.weight= weight
-        bmi_calc=weight/height**2
-        physical.bmi_calc=bmi_calc
+        bmi_calc=round(weight/height**2, 2)   
+        if (bmi_calc < 18.5):
+            bmi_category='Underweight'
+        elif (bmi_calc >= 18.5) and (bmi_calc < 25):
+            bmi_category='Healthy'
+        elif (bmi_calc >= 25) and (bmi_calc < 30):
+            bmi_category='Overweight'
+        elif (bmi_calc >= 30) and (bmi_calc < 35):
+            bmi_category='Obese'   
+        elif (bmi_calc >= 35) and (bmi_calc < 40):
+            bmi_category='Severely obese'    
+        elif (bmi_calc >= 40) and (bmi_calc < 50):
+            bmi_category='Morbidly obese'
+        elif (bmi_calc >= 50):
+            bmi_category='Super obese'
+        else:
+            bmi_category='No data available'
         form_save= form.save(commit=False)
         form_save.user= request.user
         form_save.save()
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
-        return render(request, 'bmi_result.html', {'bmi_calc':bmi_calc})
-
+        return render(request, 'bmi_result.html', {'bmi_calc':bmi_calc, 'bmi_category':bmi_category})
     return render(request, 'bmi_form.html', {'form': form})
 
 def bmi_result(request):
-    #physical=Physical.objects.order_by('user')[0]
-    #bmi_calc=physical.weight/(physical.weight**2)
     return render(request, 'bmi_result.html')
+    
+    
