01de44f8d9a85946f95d4a1ec2863c61c3d37250
EC2 Default User <ec2-user@ip-172-31-6-37.eu-west-1.compute.internal>
2020-08-17 11:34:32 +0000

    test product name from short description

--- products/tests.py
+++ products/tests.py
@@ -3,11 +3,27 @@ from .models import Product
 
 # Product model tests.
 class ProductTests(TestCase):
-    """
-    Here we'll define the tests that we'll run against our
-    Product model
-    """
+    
+    # Test setup by craeting new products
+    def setUp(self):
+        Product.objects.create(
+            name="10kg weight", 
+            category="Weights", 
+            short_description="A weight plate",
+            description="A light weight plate",
+            price ="50"
+            )
+        Product.objects.create(
+            name="pull up bar", 
+            category="Accessories", 
+            short_description="A pull up bar",
+            description="A door mounted pull up bar",
+            price ="30"
+            )
 
-    def test_str(self):
-        test_name = Product(name='Gymstuff product')
-        self.assertEqual(str(test_name), 'Gymstuff product')
+    # Test name field of product model from short descirption
+    def test_name_from_short_description(self):
+        """Name correctly identified from short_description"""
+        test_name_object_1 = Product.objects.get(short_description="A weight plate")
+        test_name_object_2 = Product.objects.get(short_description="A weight plate")
+        self.assertEqual(str(test_name_object_1), 'A weight plate')
