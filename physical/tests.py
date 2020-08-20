292d19ab9ad37a81971b38f73cf6c74d09ae96a8
chrismurray1980 <ec2-user@ip-172-31-6-37.eu-west-1.compute.internal>
2020-08-20 13:50:08 +0000

    test bmi views

--- physical/tests.py
+++ physical/tests.py
@@ -88,14 +88,20 @@ class BMI_Model_Tests(TestCase):
        # self.assertEqual(response.status_code, 200)
         #self.assertTemplateUsed(response, 'bmi_result.html')
 
-    def test_bmi_result_page(self):
-        details = Physical.objects.get(unit_type="Metric")
-        response = self.client.post(reverse('bmi_result'), details.height)
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'bmi_result.html')
-        self.assertEqual(response, 160.00)
+    #def test_bmi_result_page(self):
+     #   details = Physical.objects.get(unit_type="Metric")
+      #  response = self.client.post(reverse('bmi_result'), details.height)
+       # self.assertEqual(response.status_code, 200)
+        #self.assertTemplateUsed(response, 'bmi_result.html')
+        #self.assertEqual(response, 160.00)
+        
     
-    
+    def test_my_view(self):
+        data = {'param': 'fiesta'}
+        response = self.client.post(reverse('my-view'), data)
+        self.assertEqual(response, 'fiesta') 
+        
+        
 # Macro model tests.
 class Macro_Model_Tests(TestCase):
     
