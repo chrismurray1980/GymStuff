bb8894406843e69ae7b9747ef8db8dcabadd5226
chrismurray1980 <ec2-user@ip-172-31-6-37.eu-west-1.compute.internal>
2020-08-22 08:21:38 +0000

    test bmi views

--- physical/tests.py
+++ physical/tests.py
@@ -90,10 +90,10 @@ class BMI_Model_Tests(TestCase):
 
     def test_bmi_result_page(self):
         details = Physical.objects.get(unit_type="Metric")
-        response = self.client.post(reverse('bmi_result'), details.height)
+        response = self.client.post(reverse('bmi_result'), details)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'bmi_result.html')
-        self.assertEqual(response, 160.00)
+        self.assertEqual(response.height, 160.00)
         
     
     def test_my_view(self):
