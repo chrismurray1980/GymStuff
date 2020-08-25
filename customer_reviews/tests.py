62837db811a1e7f838aad25d86ae4c62c87c327e
chrismurray1980 <ec2-user@ip-172-31-6-37.eu-west-1.compute.internal>
2020-08-25 13:38:07 +0000

    added customer review app tests

--- customer_reviews/tests.py
+++ customer_reviews/tests.py
@@ -1,25 +1,49 @@
 from django.test import TestCase
-from products.models import Product
-from .models import customer_review
+#Import libraries views and models
+from django.test import TestCase, Client
+from .models import Product, customer_review
 
 # Customer review model tests.
 class CustomerReviewTests(TestCase):
     
     # Test setup by creating new customer review models
     def setUp(self):
-        #create user models 
-        user1 = User.objects.create(id=1, username='Bob', email='bob@example.com')
-        user2 = User.objects.create(id=2, username='Jan', email='jan@example.com')
+        #create product models 
+        product1= Product.objects.create(
+            name="10kg weight", 
+            category="Weights", 
+            short_description="A weight plate",
+            description="A light weight plate",
+            price ="50.00"
+        )
+        product2=Product.objects.create(
+            name="pull up bar", 
+            category="Accessories", 
+            short_description="A pull up bar",
+            description="A door mounted pull up bar",
+            price ="30.00"
+            )
         #create model objects
-        Physical.objects.create(
-            user = user1,
-            height = 160.00,
-            weight = 80.00,
-            unit_type = 'Metric'
-            )
-        Physical.objects.create(
-            user= user2,
-            height = 80.00,
-            weight = 200.00,
-            unit_type = 'Imperial'
-            )
+        customer_review.objects.create(
+            product = product1,
+            username = 'Jim',
+            headline = 'Great',
+            comment = 'Superb',
+            rating = 5
+        )
+        customer_review.objects.create(
+            product = product2,
+            username = 'Bob',
+            headline = 'Rubbish',
+            comment = 'Poor',
+            rating = 1
+        )
+        
+     # Test username field of customer review model from product name
+    def test_name_from_category(self):
+        """Username correctly identified from product name"""
+        test_username_object_1 = Product.objects.get(category="10kg weight")
+        test_username_object_2 = Product.objects.get(category="pull up bar")
+        self.assertEqual(str(test_username_object_1.username), 'Jim')
+        self.assertEqual(str(test_username_object_2.username), 'Bob')
+        
