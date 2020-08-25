2f26b532b33edf128635ef667496f833171757cf
chrismurray1980 <ec2-user@ip-172-31-6-37.eu-west-1.compute.internal>
2020-08-25 14:04:08 +0000

    added customer review app tests

--- customer_reviews/tests.py
+++ customer_reviews/tests.py
@@ -71,8 +71,8 @@ class CustomerReviewTests(TestCase):
         """Rating correctly identified from product """
         test_rating_object_1 = customer_review.objects.get(product=1)
         test_rating_object_2 = customer_review.objects.get(product=2)
-        self.assertEqual(str(test_rating_object_1.rating), 5)
-        self.assertEqual(str(test_rating_object_2.rating), 1) 
+        self.assertEqual(str(test_rating_object_1.rating), '5')
+        self.assertEqual(str(test_rating_object_2.rating), '1') 
         
     #Test customer review page rendered
     def test_customer_review_page(self):
